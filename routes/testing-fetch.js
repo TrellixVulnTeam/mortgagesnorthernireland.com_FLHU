1e0a418d5b4a19c9397de3f6b371e9e590b1cd16
Richard <ricky1001@hotmail.co.uk>
2022-10-14 13:01:59 +0100

    Update testing-fetch.js

--- routes/testing-fetch.js
+++ routes/testing-fetch.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.post("/testing-fetch", function(req, res){
      console.log("here r there")
-     console.log("this is the request body" + req.body) 
+     console.log("this is the request body" + req.body.firstName) 
      return res.status(200).json({
           status: 200,
           message: "Your enquiry has been received and assigned to an adviser we will contact as soon as possible"
